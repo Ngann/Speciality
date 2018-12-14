b9c4ee0134e10e86910e272aa65990929a1ae58e
Student <example@example.com>
2018-12-14 12:56:47 -0800

    add CRUD methods for product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,25 @@ class ReviewsController < ApplicationController
   def show
     @reviews = Review.find(params[:id])
   end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+  
+
+private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
+
+
 end
