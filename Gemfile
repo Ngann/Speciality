d5e1e4054f10cea9b6ea7797c7957133f396e90b
Student <example@example.com>
2018-12-15 15:39:54 -0800

    use faker gem to add products to the database

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jquery-rails'
+gem 'faker'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-products = [
-  [ "Gala Apples", 1.99 , "USA" ],
-  [ "Orange", 2 , "USA" ],
-  [ "Almond", 3 , "USA" ],
-  [ "Cashew", 4 , "USA" ],
-  [ "Breakfast Blend Coffee", 5 , "Africa" ],
-  [ "Medium Roast Coffee", 5 , "Colombia" ],
-  [ "Dark Roast Coffee", 5 , "Costa Rica" ],
-  [ "Pink Lady", 1 , "USA" ],
-  [ "Tangerine", 2 , "USA" ],
-  [ "Pastachio", 3 , "USA" ],
-  [ "Cake", 4 , "USA" ],
-]
+# Below is code for manual products we want to add unti the database
+# products = [
+#   [ "Gala Apples", 1.99 , "USA" ],
+#   [ "Orange", 2 , "USA" ],
+#   [ "Almond", 3 , "USA" ],
+#   [ "Cashew", 4 , "USA" ],
+#   [ "Breakfast Blend Coffee", 5 , "Africa" ],
+#   [ "Medium Roast Coffee", 5 , "Colombia" ],
+#   [ "Dark Roast Coffee", 5 , "Costa Rica" ],
+#   [ "Pink Lady", 1 , "USA" ],
+#   [ "Tangerine", 2 , "USA" ],
+#   [ "Pastachio", 3 , "USA" ],
+#   [ "Cake", 4 , "USA" ],
+# ]
+#
+# products.each do |name, cost, country|
+#   Product.create( name: name, cost: cost, country:country)
+# end
 
-products.each do |name, cost, country|
-  Product.create( name: name, cost: cost, country:country)
+# Below is the code using Faker to generate the data
+Product.destroy_all
+
+10.times do |index|
+  Product.create!( name: Faker::Food.fruits,
+                        cost: Faker::Number.decimal(1, 2),
+                        country: Faker::Address.country)
 end
+
+10.times do |index|
+  Product.create!( name: Faker::Coffee.blend_name,
+                        cost: Faker::Number.decimal(1, 2),
+                        country: Faker::Coffee.origin)
+end
+
+10.times do |index|
+  Product.create!( name: Faker::Food.vegetables,
+                        cost: Faker::Number.decimal(1, 2),
+                        country: Faker::Address.country)
+end
+
+p "Created #{Product.count} products"
